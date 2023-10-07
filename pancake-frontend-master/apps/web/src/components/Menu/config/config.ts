690f30be8f3fbd096ecfd3d555889925b4117621
unknown <Kartikey>
2023-10-07 20:35:44 +0530

    commit changes

---
+++ pancake-frontend-master/apps/web/src/components/Menu/config/config.ts
@@ -0,0 +1,118 @@
+import {
+  MenuItemsType,
+  DropdownMenuItemType,
+  SwapIcon,
+  SwapFillIcon,
+  EarnFillIcon,
+  EarnIcon,
+  TrophyIcon,
+  TrophyFillIcon,
+  NftIcon,
+  NftFillIcon,
+  MoreIcon,
+  DropdownMenuItems,
+} from '@pancakeswap/uikit'
+import { ContextApi } from '@pancakeswap/localization'
+import { nftsBaseUrl } from 'views/Nft/market/constants'
+import { getPerpetualUrl } from 'utils/getPerpetualUrl'
+import { SUPPORT_ONLY_BSC } from 'config/constants/supportChains'
+
+export type ConfigMenuDropDownItemsType = DropdownMenuItems & { hideSubNav?: boolean }
+export type ConfigMenuItemsType = Omit<MenuItemsType, 'items'> & { hideSubNav?: boolean; image?: string } & {
+  items?: ConfigMenuDropDownItemsType[]
+}
+
+const addMenuItemSupported = (item, chainId) => {
+  if (!chainId || !item.supportChainIds) {
+    return item
+  }
+  if (item.supportChainIds?.includes(chainId)) {
+    return item
+  }
+  return {
+    ...item,
+    disabled: true,
+  }
+}
+
+const config: (
+  t: ContextApi['t'],
+  isDark: boolean,
+  languageCode?: string,
+  chainId?: number,
+) => ConfigMenuItemsType[] = (t, isDark, languageCode, chainId) =>
+  [
+    {
+      label: t('Trade'),
+      icon: SwapIcon,
+      fillIcon: SwapFillIcon,
+      href: '/swap',
+      showItemsOnMobile: false,
+      items: [
+        {
+          label: t('Swap'),
+          href: '/swap',
+        },
+        {
+          label: t('Limit'),
+          href: '/limit-orders',
+          supportChainIds: SUPPORT_ONLY_BSC,
+          image: '/images/decorations/3d-coin.png',
+        },
+        {
+          label: t('Liquidity'),
+          href: '/liquidity',
+        },
+        
+      ].map((item) => addMenuItemSupported(item, chainId)),
+    },
+    
+    {
+      label: '',
+      href: '/info',
+      icon: MoreIcon,
+      hideSubNav: true,
+      items: [
+        {
+          label: t('Info'),
+          href: '/info',
+        },
+        {
+          label: t('IFO'),
+          href: '/ifo',
+          supportChainIds: SUPPORT_ONLY_BSC,
+          image: '/images/ifos/ifo-bunny.png',
+        },
+        {
+          label: t('Voting'),
+          href: '/voting',
+          supportChainIds: SUPPORT_ONLY_BSC,
+          image: '/images/voting/voting-bunny.png',
+        },
+        {
+          type: DropdownMenuItemType.DIVIDER,
+        },
+        {
+          label: t('Leaderboard'),
+          href: '/teams',
+          supportChainIds: SUPPORT_ONLY_BSC,
+          image: '/images/decorations/leaderboard.png',
+        },
+        {
+          type: DropdownMenuItemType.DIVIDER,
+        },
+        {
+          label: t('Blog'),
+          href: 'https://medium.com/pancakeswap',
+          type: DropdownMenuItemType.EXTERNAL_LINK,
+        },
+        {
+          label: t('Docs'),
+          href: 'https://docs.pancakeswap.finance',
+          type: DropdownMenuItemType.EXTERNAL_LINK,
+        },
+      ].map((item) => addMenuItemSupported(item, chainId)),
+    },
+  ].map((item) => addMenuItemSupported(item, chainId))
+
+export default config
